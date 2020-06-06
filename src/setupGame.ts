dcac21312953d93581b69eab8e4058ded52ec5da
Joeri Bakker <bakkerjoeri@gmail.com>
2020-06-06 18:14:00 +0200

    Generic scale-less setup

--- src/setupGame.ts
+++ src/setupGame.ts
@@ -3,34 +3,50 @@ import { Size } from './types.js';
 export function setupGame(
     containerSelector: string,
     size: Size,
-    scale = 1
 ): {
     canvas: HTMLCanvasElement;
     context: CanvasRenderingContext2D;
 } {
+    /**
+     * First, we mount the game in the container element.
+     */
     const canvas = document.createElement('canvas');
-
-    canvas.setAttribute('width', (size.width * scale * window.devicePixelRatio).toString());
-    canvas.setAttribute('height', (size.height * scale * window.devicePixelRatio).toString());
-    canvas.style.width = `${size.width * scale}px`;
-    canvas.style.height = `${size.height * scale}px`;
-
     const context = canvas.getContext('2d');
 
     if (!context) {
         throw new Error('Couldn\'t create context from canvas');
     }
 
-    context.imageSmoothingEnabled = false;
-    context.scale(window.devicePixelRatio, window.devicePixelRatio);
+    const gameContainer = document.documentElement.querySelector(containerSelector);
 
-    const gameElement = document.documentElement.querySelector(containerSelector);
-
-    if (!gameElement) {
+    if (!gameContainer) {
         throw new Error(`Couldn't find element with selector ${containerSelector} to mount canvas on.`)
     }
 
-    gameElement.appendChild(canvas);
+    gameContainer.appendChild(canvas);
+
+    /**
+     * We give the canvas the user defined pixel size through element attributes,
+     * but make sure it fills it's container through CSS.
+     */
+    canvas.setAttribute('width', (size.width).toString());
+    canvas.setAttribute('height', (size.height).toString());
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+    canvas.style.objectFit = 'contain';
+
+    /**
+     * By default an inline element, the canvas can have some stray spacing.
+     * We change its display value to block to prevent those.
+     */
+    canvas.style.display = 'block';
+
+    /**
+     * We make sure that rendering is crisp in different browsers.
+     */
+    canvas.style.imageRendering = '-moz-crisp-edges';
+    canvas.style.imageRendering = '-webkit-crisp-edges';
+    canvas.style.imageRendering = 'pixelated';
 
     return {
         context,
