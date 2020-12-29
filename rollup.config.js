258c3c99f216a5d6c1b34dc3f938aa998b5ce043
Joeri Bakker <bakkerjoeri@gmail.com>
2020-12-29 13:44:38 +0100

    Remove test stuff

--- rollup.config.js
+++ rollup.config.js
@@ -5,13 +5,13 @@ import scss from 'rollup-plugin-scss';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
-    input: 'src/example.ts',
-    output: {
-        dir: 'dist',
-        format: 'es',
-    },
-    plugins: [
-        nodeResolve(),
+	input: 'src/index.ts',
+	output: {
+		dir: 'dist',
+		format: 'es',
+	},
+	plugins: [
+		nodeResolve(),
 		typescript(),
 		vue(),
 		scss({output: 'dist/bundle.css'}),
@@ -19,5 +19,5 @@ export default {
 			'process.env.NODE_ENV': JSON.stringify('development'),
 			'process.env.VUE_ENV': JSON.stringify('browser')
 		}),
-    ],
+	],
 };
--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,5 @@ export * from './collision.js';
 export * from './events/updateAndDraw.js';
 export * from './events/keyboard.js';
 export * from './events/mouse.js';
+
+export * from './debugger/setupDebugger.js';
