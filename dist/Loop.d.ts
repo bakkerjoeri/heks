07fed50a7a6c6c6a1de7db925d2a2cec2b770618
Joeri Bakker <bakkerjoeri@gmail.com>
2021-01-29 23:37:55 +0100

    2.7.1

--- dist/Loop.d.ts
+++ dist/Loop.d.ts
@@ -1,11 +1,11 @@
 export declare class Loop {
     isRunning: boolean;
     time: number;
-    previousTime: number;
-    fps: number;
-    private update;
+    private previousTime;
+    private readonly update;
     private rafHandle;
     constructor(update: Loop['update']);
+    get fps(): number;
     start(): void;
     stop(): void;
     tick(): Promise<void>;
