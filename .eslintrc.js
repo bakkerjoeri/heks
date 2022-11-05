6e7fe19195e959a7253e607cb65959bb5dec28c8
bakkerjoeri <bakkerjoeri@gmail.com>
2022-11-05 17:10:05 +0100

    updates config & dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,12 @@
 module.exports = {
-    "parser": "@typescript-eslint/parser",
-    "plugins": [
-        "@typescript-eslint",
-        "import",
-    ],
-    "env": {
-        "browser": true,
-        "es6": true,
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-    ],
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module",
-    },
-    "rules": {
-        "quotes": [
-            "warn",
-            "single",
-        ],
-        "no-var": "warn",
-        "prefer-template": "warn",
-        "eqeqeq": "error",
-        "no-console": "off",
-        'no-prototype-builtins': "off",
-        "@typescript-eslint/no-use-before-define": ["error", { "functions": false, "classes": true }],
-        "@typescript-eslint/no-explicit-any": "off",
-		"@typescript-eslint/no-object-literal-type-assertion": "off",
-		"@typescript-eslint/no-non-null-assertion": "off",
-    }
-}
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint", "import"],
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	rules: {
+		"no-empty-function": "allow",
+	},
+};
