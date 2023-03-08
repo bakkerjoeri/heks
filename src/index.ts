3a3675e7ed4b65c33b8cdcd8f81c3d41d6091320
bakkerjoeri <bakkerjoeri@gmail.com>
2023-03-08 17:24:01 +0100

    fixes missing import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from "./GameLoopRunner.js";
 export * from "./Sprite.js";
 export * from "./canvas.js";
-export * from "./collision.js";
 export * from "./assets.js";
 export * from "./types.js";
